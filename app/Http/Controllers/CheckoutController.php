6f72f3fd3102b8a375ca6a8d467acfc8e0728d44
paras18csu153 <paras18csu153@ncuindia.edu>
2022-01-23 11:22:23 +0530

    Empty Cart on Checkout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -9,7 +9,12 @@ class CheckoutController extends Controller
 {
     public function store(Request $request){
         $products = json_decode($request['products'], true);
-        Auth::user()->orders()->create([
+        $user = Auth::user();
+
+        $cart = $user->cart;
+        $cart->products()->detach();
+
+        $user->orders()->create([
             'payment_method' => 'default',
             'address' => 'default'
         ]);
